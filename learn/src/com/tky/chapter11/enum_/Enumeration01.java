15e2a8e1bf33dc7f79bd6cea394706d216f4d229
Tky <605532779@qq.com>
2021-08-18 21:20:34 +0800

    Enumeration01 modified.

--- learn/src/com/tky/chapter11/enum_/Enumeration01.java
+++ learn/src/com/tky/chapter11/enum_/Enumeration01.java
@@ -1,4 +1,42 @@
 package com.tky.chapter11.enum_;
 
 public class Enumeration01 {
+    public static void main(String[] args) {
+        System.out.println(Season.AUTUMN);
+    }
+}
+
+//自定义类实现枚举
+class Season {
+    private String name;
+    private String desc;//描述
+
+    public static final Season SPRING = new Season("春天","温暖");
+    public static final Season SUMMER = new Season("夏天","炎热");
+    public static final Season AUTUMN = new Season("秋天","凉爽");
+    public static final Season WINTER = new Season("冬天","寒冷");
+
+    //1.构造器私有化，防止直接new对象
+    //2.去掉set方法，防止属性被修改
+    //3.在Season内部，直接创建固定的对象
+    private Season(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    @Override
+    public String toString() {
+        return "Season{" +
+                "name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                '}';
+    }
 }
