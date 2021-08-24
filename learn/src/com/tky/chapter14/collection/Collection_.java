5bc466a2d4bb207f2c3165ec2989b93e015c1a42
Tky <605532779@qq.com>
2021-08-24 21:32:10 +0800

    New files added.

--- learn/src/com/tky/chapter14/collection/Collection_.java
+++ learn/src/com/tky/chapter14/collection/Collection_.java
@@ -1,9 +1,29 @@
 package com.tky.chapter14.collection;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * @author tky
  * @version 1.0
  * @date 2021/8/24 21:13
  */
 public class Collection_ {
+    @SuppressWarnings("all")
+    public static void main(String[] args) {
+//        Collection
+//        Map
+
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("jack");
+        arrayList.add("tom");
+        arrayList.add("tky");
+        System.out.println(arrayList);
+
+        HashMap hashMap = new HashMap();
+        hashMap.put("No1", "北京");
+        hashMap.put("No2", "上海");
+        hashMap.put("No3", "广州");
+        System.out.println(hashMap);
+    }
 }
---
+++ learn/src/java/util/Iterator.java
@@ -0,0 +1,9 @@
+package java.util;
+
+/**
+ * @author tky
+ * @version 1.0
+ * @date 2021/8/24 21:16
+ */
+public interface Iterator {
+}
